d3f5e5a0894ed0c0260502ab3398e3dcd2d1929c
Olga <1000da@bk.ru>
2022-11-25 17:21:13 +0500

    fixed css carousel

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,
-/* 		adaptiveHigth: true, */
+		adaptiveHigth: true,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows_back.png"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows_next.png"></button>',
 		responsive: [
