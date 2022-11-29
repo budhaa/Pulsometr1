e3768a48986abc56c8f2123de824116bcaec16d2
Olga <1000da@bk.ru>
2022-11-29 12:59:43 +0500

    fixed section carouselwith  Tiny Slider 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function(){
+/* $(document).ready(function(){
 	$('.carousel__inner').slick({
 		speed: 1200,
 		adaptiveHigth: true,
@@ -15,4 +15,21 @@ $(document).ready(function(){
 			}
 		]
 	  });
-});
+}); */
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+	controls: false,
+	nav: false
+  });
+
+  document.querySelector('.prev').addEventListener('click', function () {
+	slider.goTo('prev');
+  });
+
+  document.querySelector('.next').addEventListener('click', function () {
+	slider.goTo('next');
+  });
